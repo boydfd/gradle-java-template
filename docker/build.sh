4dc9593aa75fc55f88b13c917b19294d1a54d639
boydfd <boydfd@163.com>
2020-04-13 00:30:43 +0800

    [@boydfd] #N/A rm user and password in docker build.sh .

--- docker/build.sh
+++ docker/build.sh
@@ -6,9 +6,7 @@ cd $(dirname $([ -L $0 ] && readlink -f $0 || echo $0))
 
 
 set -x
-echo $docker_user
-echo $docker_password
-docker login $dockerRegistry -u $docker_user -p $docker_password
+docker login ${dockerRegistry} -u ${docker_user} -p ${docker_password}
 docker build -t "$dockerRegistry/$imageName" .
 docker push "$dockerRegistry/$imageName"
 set +x
