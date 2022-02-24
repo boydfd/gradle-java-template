473391fac9b769317ebd5648c6881763dfc633f9
boydfd <boydfd@163.com>
2022-02-24 11:09:57 +0800

    [@aboaydfd] upgrade to gradle 7.4 and add spring web.

--- src/test/java/com/aboydfd/AppTest.java
+++ src/test/java/com/aboydfd/AppTest.java
@@ -3,12 +3,6 @@ package com.aboydfd;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.PrintStream;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
 class AppTest {
     @Test
